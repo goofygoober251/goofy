845cd267665172046d2e61c29bb864208f5ebfcd
goofygoober251 <126897692+goofygoober251@users.noreply.github.com>
2023-03-13 10:59:48 -0400

    made code work

---
+++ images.g.ts
@@ -0,0 +1,109 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Q0":
+            case "Captain Creativity":return img`
+.....fffffff.....
+...ff8888888ff...
+..f38888888883f..
+..f33333333333f..
+.f3333f3333f333f.
+.f3333333333333f.
+.f8888888888888f.
+.f8888888888888f.
+.f8888888888888f.
+.f8888888888888f.
+.f888f88888f888f.
+..f888fffff888f..
+..f88888888888f..
+...f888888888f...
+....ff55555ff....
+...f555555555f...
+..f55555995555f..
+.f33f5595555f33f.
+f333f5595555f333f
+f33ff5559955ff33f
+.ff.f55fff55f.ff.
+...f77f...f77f...
+...f77f...f77f...
+..f777f...f777f..
+..fffff...fffff..
+`;
+            case "image1":
+            case "Big Bore":return img`
+.....fffffff.....
+...ff5555555ff...
+..f88888888888f..
+..f8ff58885ff8f..
+.f8855f888f5588f.
+.f885f58885f588f.
+.f8888888888888f.
+.f5555555555555f.
+.f5555555555555f.
+.f5555555555555f.
+.f5555555555555f.
+..f555fffff555f..
+..f55555555555f..
+...f555555555f...
+....ffcccccff....
+...fccc22ccccf...
+..fcccc2c2ccccf..
+.f99fcc22cccf99f.
+f999fcc2c2ccf999f
+f99ffcc22cccff99f
+.ff.fccfffccf.ff.
+...f99f...f99f...
+...f99f...f99f...
+..f999f...f999f..
+..fffff...fffff..
+`;
+            case "image2":
+            case "Foolish Follower":return img`
+.....fffffff.....
+...ff7777777ff...
+..f77777777777f..
+..f77777777777f..
+.f7777777777777f.
+.f777f7777f7777f.
+.f7777777777777f.
+.f7777777777777f.
+.f777fffffff777f.
+.f7777777777777f.
+.f7777777777777f.
+..ff777777777ff..
+....ff77777ff....
+...fcccccccccf...
+..fcccc222ccccf..
+.f22fcc2ccccf22f.
+f222fcc222ccf222f
+f22ffcc2ccccff22f
+.ffffcc2ccccf.ff.
+....fccfffccf....
+...f22f...f22f...
+...f22f...f22f...
+..f222f...f222f..
+..fffff...fffff..
+.................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
